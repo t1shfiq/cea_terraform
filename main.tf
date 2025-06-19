5783c2cd750a1b7db4038d269ae2d69e6d0049f8
Ali Tashfiq Wazed <alitashfiq@Alis-MacBook-Pro.local>
2025-06-19 17:21:54 -0400

    setup main.tf with vpc, subnet, igw, route table

---
+++ main.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#VPC
+resource "aws_vpc" "main" {
+  cidr_block = "192.168.0.0/16"
+
+  tags = {
+    Name = "main-tf-vpc"
+  }
+}
+
+# Subnet 1
+resource "aws_subnet" "Subnet1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "192.168.1.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "Subnet 1"
+    }
+}
+
+# Subnet 2
+resource "aws_subnet" "Subnet2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "192.168.2.0/24"
+    availability_zone = "us-east-2b"
+
+    tags = {
+        Name = "Subnet 2"
+    }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.owner_id
+
+  tags = {
+    Name = "main-vpc-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+# Route Table Associate Subnet 1
+
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.Subnet1.id
+  route_table_id = aws_route_table.route_table.id
+
+
+}
+
+
+# Route Table Associate Subnet 2
+
+resource "aws_route_table_association" "b" {
+  subnet_id = aws_subnet.Subnet2.id
+  route_table_id = aws_route_table.route_table.id
+
+}
